0a2a024f6014bf926a8d7608e9a5de3a336704fa
underwatergrasshopper <underwater.grasshopper@gmail.com>
2022-09-16 16:11:16 +0200

    Added basic functionality. Added initial tests.

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,8 +1,26 @@
-#include <stdio.h>
 #include "TrivialTestKit.h"
 
+#include <stdio.h>
+#include <stdlib.h> 
+#include <assert.h>
+
 int main() {
-    puts("Hello.");
-    test();
+    system("if not exist log mkdir log");
+
+    {
+        const std::string filename          = "log/SaveLoadTest.txt";
+        const std::string expected_content  = "Some text to save.";
+
+        assert(TTK_SaveContentToFile(filename, expected_content));
+
+        bool is_success = false;
+        const std::string content = TKK_LoadContentFromFile(filename, &is_success);
+        assert(is_success);
+        assert(content == expected_content);
+    }
+
+    TTK_Assert(1 == 2);
+    TTK_AssertM(3 == 5, "This is stupid.");
+
     return 0;
 }
--- TrivialTestKit/include/TrivialTestKit.h
+++ TrivialTestKit/include/TrivialTestKit.h
@@ -27,14 +27,78 @@
  * @details This file is a part of project: TrivialTestKit. Can be distributed separately.
  */
 
+// Targeted language version: C++11.
+
 #ifndef TRIVIALTESTKIT_H_
 #define TRIVIALTESTKIT_H_
 
 #include <stdio.h>
+#include <string>
 
-inline void test() {
-    puts("test");
+
+struct TTK_Data {
+    FILE* communicate_output;
+};
+
+inline TTK_Data& TTK_ToData() {
+    static TTK_Data s_data = { stdout };
+    return s_data;
 }
+
+inline void TTK_SetCommunitateOut(FILE* communicate_output) {
+    TTK_ToData().communicate_output = communicate_output;
+}
+
+inline void TKK_CommunicateAssertFail(FILE* communicate_output, unsigned line, const char* condition, const char* filename, const char* message) {
+    if (communicate_output) {
+        if (message) {
+            fprintf(communicate_output, "[fail] [line:%d] [file:%s] [condition:%s] [message:%s] \n", line, filename, condition, message);
+        } else {
+            fprintf(communicate_output, "[fail] [line:%d] [file:%s] [condition:%s]\n", line, filename, condition);
+        }
+        fflush(communicate_output);
+    }
+}
+
+inline void TKK_CommunicateAssertFail(FILE* communicate_output, unsigned line, const char* condition, const char* file_name, const std::string& message) {
+    TKK_CommunicateAssertFail(communicate_output, line, condition, file_name, message);
+}
+
+// filename         Name of the file from which content will be loaded.
+// is_success       (optional) If is not nullptr then stores at pointer location: true - when content has been loaded from file, false - otherwise.
+// return           Content of the file.
+inline std::string TKK_LoadContentFromFile(const std::string& filename, bool* is_success = nullptr) {
+    std::string content;
+
+    FILE* file = nullptr;
+    if (fopen_s(&file, filename.c_str(), "r") == 0 && file) {
+        char c;
+        while ((c = fgetc(file)) != EOF) {
+            content += c;
+        }
+        fclose(file);
+
+        if (is_success) *is_success = true;
+    } else {
+        if (is_success) *is_success = false;
+    }
+
+    return content;
+}
+
+inline bool TTK_SaveContentToFile(const std::string& filename, const std::string& content) {
+    FILE* file = nullptr;
+    if (fopen_s(&file, filename.c_str(), "w") == 0 && file) {
+        if (fwrite(content.c_str(), sizeof(char), content.length(), file) == content.length()) {
+            fclose(file);
+            return true;
+        }
+    }
+    return false;
+}
+
+#define TTK_Assert(condition) if (!(condition)) { TKK_CommunicateAssertFail(TTK_ToData().communicate_output, __LINE__, #condition, __FILE__, nullptr); } (void)0
+#define TTK_AssertM(condition, message) if (!(condition)) { TKK_CommunicateAssertFail(TTK_ToData().communicate_output, __LINE__, #condition, __FILE__, message); } (void)0
 
 #endif // TRIVIALTESTKIT_H_
 

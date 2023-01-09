2d4be3078083529863820f2f1889e9cf12e2d7b6
underwatergrasshopper <underwater.grasshopper@gmail.com>
2023-01-09 05:35:53 +0100

    Removed MinGW support.

--- TestMinGW_32.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-set ACTION=%1
-if "%ACTION%" equ "" set ACTION=run
-
-cd Test
-call MinGW_Make.bat !ACTION! Release 32 C:\llvm-mingw-20220906-ucrt-i686\bin
-cd ..
--- TestMinGW_64_Wide.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-set ACTION=%1
-if "%ACTION%" equ "" set ACTION=run
-
-cd Test
-call MinGW_Make.bat !ACTION! ReleaseWide 64 C:\llvm-mingw-20220906-ucrt-x86_64\bin
-cd ..
